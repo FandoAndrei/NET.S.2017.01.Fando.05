9605c2af1554e90b9dee720b14556705784fb24e
FandoAndrei <fando.andrei@gmail.com>
2017-03-23 13:41:53 +0300

    Euclidean algorithm added
    Euclidean algorithm class library designed

---
+++ MathMethods/MathMethod/MathMethod.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematiciansMethods
+{
+    class MathMethod
+    {
+
+        #region Public members part
+
+
+
+        public static int EuclideanGCDMethodCall(int a, int b)
+        {
+           return EuclideanGCDMethod(a,b);
+        }
+
+        public static int EuclideanGCDMethodCall(int a, int b, int c)
+        {
+           return EuclideanGCDMethod(EuclideanGCDMethod(a,b),c);
+        }
+
+        /// <summary>
+        /// This method'll be called when nomber of GCD values is greater than 3.
+        /// </summary>
+        /// <param name="values">This is an array of values.</param>
+        /// <returns>The GCD of entire array <param>values</param>. If number of <param>values</param> equals to 0 returns (-1) </returns>
+
+        public static int EuclideanGCDMethodCall(params int[] values)
+        {
+
+            int gcd;
+            if (values.Length!=0)
+            {
+                gcd = values[0];
+                for (int i = 1; i < values.Length; i++)
+                {
+                    gcd = EuclideanGCDMethod(gcd, values[i]);
+                }
+                return gcd;
+            }
+            else return -1;      
+        }
+
+        #endregion;
+
+        #region Private members part
+
+        /// <summary>
+        /// EuclideanGCDMethod is a method for counting the Greatest Common Devisor(GCD) of two integers.
+        /// As soon as there is no difference between GCD of positive or negative values, we make them both positive.
+        /// </summary>
+        /// <returns>The GCD of values (a,b), if any of them equals to 0, returns -1</returns>
+        private static int EuclideanGCDMethod(int value_a, int value_b)
+        {
+            if (IsValid(value_a, value_b))
+            {
+                if (value_a < 0)
+                    value_a *= (-1);
+                if (value_b < 0)
+                    value_b *= (-1);
+
+                while (value_a != value_b)
+                {
+                    if (value_a > value_b)
+                        value_a = value_a - value_b;
+                    else
+                        value_b = value_b - value_a;
+                }
+                return value_a;
+            }
+            return -1;
+        }
+
+        #endregion
+
+        #region Imput validation methods
+
+        /// <summary>
+        /// Validates imput data for evaluation method.
+        /// </summary>
+        /// <param name="value_a">first number</param>
+        /// <param name="value_b">second number</param>
+        /// <returns>True if nine of <param>a</param> and <param>b</param> equals to 0, other way - False</returns>
+
+        private static bool IsValid(int value_a, int value_b)
+        {
+            if (value_a != 0)
+                if (value_b != 0)
+                    return true;
+                else
+                    return false;
+            else
+                return false;
+        }
+
+        #endregion
+
+    }
+}
--- MathMethods/MathMethod/Properties/AssemblyInfo.cs
+++ MathMethods/MathMethod/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EuclideanMethod")]
+[assembly: AssemblyTitle("ClassLibrary1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EuclideanMethod")]
+[assembly: AssemblyProduct("ClassLibrary1")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6060ab6b-1084-4136-b5b7-b59d927d2efa")]
+[assembly: Guid("82f12a16-653c-4107-a4ca-5677a1178f1e")]
 
 // Version information for an assembly consists of the following four values:
 //
